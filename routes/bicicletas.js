1741adeccaf539732c6a7b364891ff730930b95d
Eulises <ecortescp@gmail.com>
2024-06-09 22:14:42 -0400

    Agregamos POST a bicicletas y uuid

--- routes/bicicletas.js
+++ routes/bicicletas.js
@@ -1,10 +1,11 @@
-import { Router, query } from 'express'
-import * as db from '../db/index.js'
+import { Router, query } from 'express';
+import * as db from '../db/index.js';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = Router()
 
 //Obtener todas las bicicletas
-router.get("/", async (req, res) =>{
+router.get("/", async (req, res) => {
     try {
         const text = 'SELECT * FROM bicicletas'
         const result = await db.query(text)
@@ -20,4 +21,37 @@ router.get("/", async (req, res) =>{
         res.send("No encontramos bicicletas")
     }
 })
+
+router.post("/", async (req, res) => {
+    const {marca, modelo, precio} = req.body
+
+    if(marca && modelo && precio){
+        //Solicitud correcta
+        try {
+            const text = 'INSERT INTO bicicletas (id, marca, modelo, precio) VALUES ($1, $2, $3, $4) RETURNING *'
+            const values = [uuidv4(), marca, modelo, Number[precio]]
+
+            const result = await db.query(text, values)
+
+            res.status(201).json({
+                status: 201,
+                message: "Bicicleta creada con éxito",
+                bicicleta: result.rows
+            })
+        } catch (error) {
+            res.status(500).json({
+                status: 500,
+                message: 'Error interno de servidor'   
+            })
+        }
+    }else {
+        //Solicitud erroneo
+        res.status(400).json({
+            status: 400,
+            message: "Bad request"
+        })
+    }
+})
+
+
 export {router}
